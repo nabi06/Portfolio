62de78763b0b21c242562334b0af87c0e1b02d20
Niyaz Nabi <niyaznabi06@gmail.com>
2025-06-13 16:23:53 -0500

    New commit

--- src/once-ui/components/SmartImage.tsx
+++ src/once-ui/components/SmartImage.tsx
@@ -73,7 +73,7 @@ const SmartImage: React.FC<SmartImageProps> = ({
         };
     };
 
-    const isVideo = src.endsWith('.mp4');
+    const isVideo = src && typeof src === 'string' ? src.endsWith('.mp4') : false;
 
     return (
         <>
